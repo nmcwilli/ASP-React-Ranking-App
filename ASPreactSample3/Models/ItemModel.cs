0929511818959eebf337c08e7cecd20b633b529f
mcwil <mcwil@DESKTOP-8ELGT17>
2023-07-01 13:54:13 -0300

    Add project files.

---
+++ ASPreactSample3/Models/ItemModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ASPreactSample3.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int ImageId { get; set; }
+
+        public int Ranking { get; set; }
+
+        public int ItemType { get; set; } 
+    }
+}
